c4c6a934daa5434dcf37a97efd0a26b159159f02
dwnppoalt <95208803+dwnppoalt@users.noreply.github.com>
2021-11-29 19:42:06 +0800

    Update nhentai.py

--- nhentai.py
+++ nhentai.py
@@ -2,42 +2,38 @@ import random
 import requests
 import time
 import os
-error = int(0)
-wrkng = int(0)
+error = 0
+wrkng = 0
 num = int(input("How Many? - "))
 name = input("Name of output text file? - ")
 print("CAUTION: Sending can be slow if your internet connection is slow.")
 start = time.time()
-end = time.time()
+
 for i in range(num):
-    True
-    while True:
-        outF = open(name + ".txt", "a")
-        seed = random.sample(range(374305), 1)
-        if(num == wrkng):
+    outF = open(name + ".txt", "a")
+    seed = random.sample(range(374305), 1)
+    if(num == wrkng):
             break
-        url = "http://nhentai.to/g/" + str(seed)
-        s = url
-        c = seed
-        fl = url
-        s1 = s.replace("[","")
-        s2 = s1.replace("]", "")
-        r = requests.get(s2)
-        if r.status_code == 404:
+    url = "http://nhentai.to/g/" + str(seed)
+    s = url
+    s1 = s.replace("[","").replace("]","")
+    r = requests.get(s1)
+    if r.status_code == 404:
             error += 1
             errurl = url
-        else:
-            outF.writelines(str(s2))
+    else:
+            outF.writelines(str(s1))
             outF.write('\n')
             wrkng += 1
         
-        if os.name == 'posix':
+    if os.name == 'posix':
             _ = os.system('clear')
-        else:
+    else:
             _ = os.system('cls')
-        print("Counter:")
-        print("Error: " + str(error))
-        print("Completed: " + str(wrkng))
+    print("Counter:")
+    print("Error: " + str(error))
+    print("Completed: " + str(wrkng))
+    outF.close()
 
  
 end = time.time()
